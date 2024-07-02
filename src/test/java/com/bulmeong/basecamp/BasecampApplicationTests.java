23df014e1faca1f2043c11ac53d0319c0c3f5984
fossil-jjh <74957088+fossil-jjh@users.noreply.github.com>
2024-07-02 13:54:02 +0900

    init

---
+++ src/test/java/com/bulmeong/basecamp/BasecampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulmeong.basecamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasecampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
