2805d6fb126f7ddf749fb982d499cac56150f1d8
fossil <74957088+fossil-jjh@users.noreply.github.com>
2024-07-10 13:03:19 +0900

    유틸 사용법 임시 표기

--- src/main/java/com/bulmeong/basecamp/camp/controller/CampsiteController.java
+++ src/main/java/com/bulmeong/basecamp/camp/controller/CampsiteController.java
@@ -1,12 +1,24 @@
 package com.bulmeong.basecamp.camp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.bulmeong.basecamp.common.util.Utils;
+
 @Controller
 public class CampsiteController {
+
+    //유틸 사용법 1 : 유틸을 선언한다
+    @Autowired
+    private Utils util;
+
     @RequestMapping("camp/unity")
     public String unity(){
+        //유틸 사용법 2 : 필요한 페이지에 로그인 유저를 추가한다 
+        //유틸 사용법 3 : 세션에서 'sessionUserInfo'로 UserDto를 가져올 수 있다
+        util.loginUser();
+
         return "camp/unity";
     }
 }
