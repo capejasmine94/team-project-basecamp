7856613df4b73a365ec5299605c7c392cb7ce842
sieunyi <sieun427@gmail.com>
2024-07-11 12:06:17 +0900

    소모임생성(가입조건 추가)

--- src/main/java/com/bulmeong/basecamp/club/mapper/ClubSqlMapper.java
+++ src/main/java/com/bulmeong/basecamp/club/mapper/ClubSqlMapper.java
@@ -3,10 +3,11 @@ package com.bulmeong.basecamp.club.mapper;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.bulmeong.basecamp.club.dto.ClubDto;
+import com.bulmeong.basecamp.club.dto.ClubJoinConditionDto;
 
 
 @Mapper
 public interface ClubSqlMapper {
     public void insertClubDto(ClubDto clubDto);
-   
+    public void insertClubJoinCondition(ClubJoinConditionDto clubJoinConditionDto);
 }
