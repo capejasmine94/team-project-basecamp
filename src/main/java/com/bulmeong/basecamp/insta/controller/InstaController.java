5283050ebcdf06085f3f62d82cec45284b388e31
nhj1013 <nhj6358@gmail.com>
2024-07-11 09:56:46 +0900

    html추가

--- src/main/java/com/bulmeong/basecamp/insta/controller/InstaController.java
+++ src/main/java/com/bulmeong/basecamp/insta/controller/InstaController.java
@@ -1,7 +1,23 @@
 package com.bulmeong.basecamp.insta.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.bulmeong.basecamp.common.util.Utils;
 
 @Controller
+@RequestMapping("insta")
 public class InstaController {
+    @Autowired
+    private Utils util;
+
+    // 고객이 인스타 최초 접속시 나타나는 Page
+    @RequestMapping("confirmPage")
+    public String confirmPage(){
+        util.loginUser();
+
+        return "insta/confirmPage";
+    }
+
 }
