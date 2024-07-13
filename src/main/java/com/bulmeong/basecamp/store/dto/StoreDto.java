dc32ec3b433d9b7de93ac51518cc0d168cf0b33b
qchod1234 <qchod@naver.com>
2024-07-13 20:03:39 +0900

    html 하는중~~

--- src/main/java/com/bulmeong/basecamp/store/dto/StoreDto.java
+++ src/main/java/com/bulmeong/basecamp/store/dto/StoreDto.java
@@ -1,4 +1,22 @@
 package com.bulmeong.basecamp.store.dto;
 
+import lombok.Data;
+import java.util.Date;
+
+@Data
 public class StoreDto {
+    private int id;
+    private String account_id;
+    private String account_pw;
+    private String seller;
+    private String phone;
+    private String email;
+    private String name;
+    private int crn;
+    private String address;
+    private String web_link;
+    private String description;
+    private String main_image;
+    private String status;
+    private Date created_at;
 }
