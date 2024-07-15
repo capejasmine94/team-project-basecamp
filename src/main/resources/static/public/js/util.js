c1ebd7e899d264d837bd7414cc6ec9674bb9bf32
fossil-jjh <74957088+fossil-jjh@users.noreply.github.com>
2024-07-15 12:31:44 +0900

    팝업에 선택지 1개짜리 유틸 추가

--- src/main/resources/static/public/js/util.js
+++ src/main/resources/static/public/js/util.js
@@ -1,6 +1,11 @@
+//팝업을 쉽게 만들어줍니다.
+//popup : 두개의 선택지가 있는 팝업입니다
+//popupYesOrYes : 하나의 선택지만 있는 팝업입니다.
 //1. <div th:replace="~{common/navi::popup}"></div> 를 html에 추가합니다.
-//2. popup을 onclick에 넣으면 됩니다
-//3. popup의 매개변수는 (제목,내용,예,아니오,function(){예 눌렀을 떄 액션}) 순으로 넣으시면 됩니다.
+//2. 확인을 눌렀을 때 실행하는 함수를 만듭니다.
+//3. popup을 onclick에 넣습니다.
+//4-1. popup의 매개변수는 (제목,내용,예,아니오,function(){2번에서 만든 함수}) 순으로 넣으시면 됩니다.
+//4-2. popupYesOrYes의 매개변수는 기존 popup에서 아니오만 빼면 됩니다.
 
 function popup(title,content,yes,no,action) {
    const node = document.getElementById('popup');
@@ -9,10 +14,25 @@ function popup(title,content,yes,no,action) {
    text = node.querySelector("#popup-content");
    text.innerText = content;
    text = node.querySelector("#popup-no");
+   text.classList.remove('d-none');
    text.innerText = no;
    text = node.querySelector("#popup-button");
    text.onclick = action;
    text.innerText = yes;
+   bootstrap.Modal.getOrCreateInstance('#popup').show();
+}
+
+function popupYesOrYes(title,content,accept,action) {
+   const node = document.getElementById('popup');
+   let text = node.querySelector("#popup-title");
+   text.innerText = title;
+   text = node.querySelector("#popup-content");
+   text.innerText = content;
+   text = node.querySelector("#popup-no");
+   text.classList.add('d-none');
+   text = node.querySelector("#popup-button");
+   text.onclick = action;
+   text.innerText = accept;
    bootstrap.Modal.getOrCreateInstance('#popup').show();
 }
 
