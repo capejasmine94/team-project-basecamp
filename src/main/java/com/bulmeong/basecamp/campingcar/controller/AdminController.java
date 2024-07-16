a15f10a0cdd79770b1db52d5e19b06c20142556a
yangyang1003 <yangeunja1003@gmail.com>
2024-07-16 11:48:46 +0900

    아직도 차량등록이 멀었네...또륵

--- src/main/java/com/bulmeong/basecamp/campingcar/controller/AdminController.java
+++ src/main/java/com/bulmeong/basecamp/campingcar/controller/AdminController.java
@@ -8,31 +8,41 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("admin")
 public class AdminController {
 
-    @RequestMapping("productRegister")
-    public String productRegister() {              
-
-        return "admin/productRegister";
-    }
 // 판매자페이지 main
     @RequestMapping("main")
     public String main(){
         
         return "admin/main";
     }
-// admin_main에 sub_category_예약현황 쓰는 방식
+// admin_main에 sub_category_쓰는 방식
     @RequestMapping(value = "/carRegister", method = RequestMethod.POST)
     public String reservationStatus() {
         return "admin/carRegister";
     }
 
-// 내가 만든 main용 
-    @RequestMapping("maintest")
-    public String maintest() {
-        return "admin/maintest";
-    }
-// 파트너 유형 test용
-    @RequestMapping("sellerTypeLoginpageTest")
-    public String test() {
-        return "admin/sellerTypeLoginpageTest";
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+// // 내가 만든 main용 
+//     @RequestMapping("maintest")
+//     public String maintest() {
+//         return "admin/maintest";
+//     }
 }
