69f6d58d20d63507c7639070bf43f3ef647cd1df
fossil-jjh <74957088+fossil-jjh@users.noreply.github.com>
2024-07-19 19:38:13 +0900

    Update CampsiteController.java

--- src/main/java/com/bulmeong/basecamp/camp/controller/CampsiteController.java
+++ src/main/java/com/bulmeong/basecamp/camp/controller/CampsiteController.java
@@ -1,21 +1,14 @@
 package com.bulmeong.basecamp.camp.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.bulmeong.basecamp.camp.dto.CampsiteBankDto;
 import com.bulmeong.basecamp.camp.dto.CampsiteDto;
-import com.bulmeong.basecamp.common.util.Utils;
 
 @RequestMapping("camp")
 @Controller
 public class CampsiteController {
-
-    //유틸 사용법 1 : 유틸을 선언한다
-    @Autowired
-    private Utils utils;
 
     @RequestMapping("unity")
     public String unity(){
