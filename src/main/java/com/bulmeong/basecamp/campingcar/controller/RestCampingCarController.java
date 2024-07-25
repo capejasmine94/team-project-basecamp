1142233011b2ebb6983de447d0e917f5f5aabc2a
yangyang1003 <yangeunja1003@gmail.com>
2024-07-25 19:09:28 +0900

    RestCampingCarController 수정

--- src/main/java/com/bulmeong/basecamp/campingcar/controller/RestCampingCarController.java
+++ src/main/java/com/bulmeong/basecamp/campingcar/controller/RestCampingCarController.java
@@ -1,9 +1,13 @@
 package com.bulmeong.basecamp.campingcar.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bulmeong.basecamp.campingcar.dto.RestResponseDto;
 import com.bulmeong.basecamp.campingcar.service.AdminService;
 
 @RestController
@@ -13,6 +17,15 @@ public class RestCampingCarController {
     @Autowired 
     private AdminService adminService;
 
+    @RequestMapping("campingcarList")
+    public RestResponseDto campingcarList() {
+        RestResponseDto restResponseDto = new RestResponseDto();
+        restResponseDto.setResult("success");
+        
+        // 캠핑카 등록 list 출력
+        List<Map<String,Object>> campingcarList = adminService.getCampingCarAll();
+        restResponseDto.add("campingcarList", campingcarList);
 
-
+        return restResponseDto;   
+    }
 }
