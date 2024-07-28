b9396f19f0d7e291342305428ebd4bd66ba2a329
yangyang1003 <yangeunja1003@gmail.com>
2024-07-29 04:29:45 +0900

    partner : carManagement List 출력

--- src/main/java/com/bulmeong/basecamp/campingcar/controller/RestCampingCarController.java
+++ src/main/java/com/bulmeong/basecamp/campingcar/controller/RestCampingCarController.java
@@ -7,8 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bulmeong.basecamp.campingcar.dto.RentalCompanyDto;
 import com.bulmeong.basecamp.campingcar.dto.RestResponseDto;
 import com.bulmeong.basecamp.campingcar.service.PartnerCampingCarService;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("api/campingcar")
@@ -30,13 +33,15 @@ public class RestCampingCarController {
     }
 
     @RequestMapping("campingCarManagement")
-    public RestResponseDto campingCarManagement(int rental_company_id) {
+    public RestResponseDto campingCarManagement(HttpSession session) {
         RestResponseDto restResponseDto = new RestResponseDto();
         restResponseDto.setResult("success");
-        // 판매자 : 캠핑카 등록 현황 및 수정 List 출력
-        List<Map<String,Object>> carBySellerIdList = partnerCampingCarService.getCampingcarBySellerId(rental_company_id);
-        restResponseDto.add("carBySellerIdList", carBySellerIdList);
+        RentalCompanyDto rentalCompanyDto =(RentalCompanyDto)session.getAttribute("sessionCaravanInfo");
 
+        // 판매자 : 캠핑카 등록 현황 및 수정 List 출력
+        List<Map<String,Object>> registeredCampersList = partnerCampingCarService.getCampingcarBySellerId(rentalCompanyDto.getId());
+        restResponseDto.add("registeredCampersList", registeredCampersList);
+        
         return restResponseDto;
     }
 }
