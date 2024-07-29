cb976805d18041f2b6f4564e186f51e89abc732e
qchod1234 <qchod@naver.com>
2024-07-29 20:51:34 +0900

    Create UserDeliveryInfoDto.java

---
+++ src/main/java/com/bulmeong/basecamp/store/dto/UserDeliveryInfoDto.java
@@ -0,0 +1,13 @@
+package com.bulmeong.basecamp.store.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDeliveryInfoDto {
+    private int id;
+    private int user_id;
+    private String receiver_name;
+    private String phone;
+    private String delivery_address;
+    private boolean is_default_address;
+}
