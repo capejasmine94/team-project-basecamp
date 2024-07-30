869a17b44f53ce9113bb8532b9ff77850990d7fa
fossil <74957088+fossil-jjh@users.noreply.github.com>
2024-07-30 10:11:36 +0900

    mapUtil추가
    주소지를 지도에 띄운다. 카카오맵 API 사용함

---
+++ src/main/resources/static/public/js/mapUtil.js
@@ -0,0 +1,39 @@
+// 사용하는 html에 스크립트 추가 :          일단 제 Api key를 사용했습니다.나중에 ↓ 이 부분 안에 들어가는 키를 바꾸셔야 됩니다
+// <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=d17362e8bc72e774ada9174ac4347761&libraries=services"></script>
+
+// 지도가 들어가는 곳에 <div id="map"></div> 추가
+// 스크립트로 showMap(주소)를 쓰면 지도에 주소위치가 땋 뜹니다
+
+// 지도를 생성합니다    
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+mapOption = {
+    center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+    level: 3 // 지도의 확대 레벨
+};  
+const map = new kakao.maps.Map(mapContainer, mapOption); 
+
+
+function showMap(address){
+// 주소-좌표 변환 객체를 생성합니다
+const geocoder = new kakao.maps.services.Geocoder();
+
+// 주소로 좌표를 검색합니다
+geocoder.addressSearch(address, function(result, status) {
+
+    // 정상적으로 검색이 완료됐으면 
+    if (status === kakao.maps.services.Status.OK) {
+
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        const marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+    } 
+});    
+map.relayout();
+}
