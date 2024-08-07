69a1b8c2802656831271a2e311b7b03812c2b7c0
yangyang1003 <yangeunja1003@gmail.com>
2024-08-07 16:20:33 +0900

    RentUser DTO 생성

---
+++ src/main/java/com/bulmeong/basecamp/campingcar/dto/RentUserDto.java
@@ -0,0 +1,17 @@
+package com.bulmeong.basecamp.campingcar.dto;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class RentUserDto {
+    private int id;
+    private int user_id;
+    private String driver_license_number;
+    private int driver_license_id;
+    private int driver_age_id;
+    private int driving_experience_id;
+    private String driver_license_file;
+    private Date created_at;
+}
